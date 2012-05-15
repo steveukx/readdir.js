fd81c161863b22b2506f0a1d69f08d689cafd46e
Steve King <steve@kwsites.co.uk>
2012-05-15 19:01:24 +0100

    Improved filtering & items in result array don't include the base path

--- test/test.js
+++ test/test.js
@@ -5,25 +5,68 @@ var Assert = require('assert');
    var readSync = require('../lib/readdir.js').readSync;
 
    var everyFile = readSync('example_dir');
-   Assert.deepEqual(everyFile, [ 'example_dir/AAA/aaa.js',
-      'example_dir/AAA/aaa.txt',
-      'example_dir/abc.js',
-      'example_dir/abc.txt',
-      'example_dir/BBB/bbb.js',
-      'example_dir/BBB/bbb.txt',
-      'example_dir/CCC/ccc.js',
-      'example_dir/CCC/ccc.txt',
-      'example_dir/CCC/DDD/ddd.js',
-      'example_dir/CCC/DDD/ddd.txt' ]);
+   Assert.deepEqual(everyFile, [ 'AAA/aaa.js',
+      'AAA/aaa.txt',
+      'abc.js',
+      'abc.txt',
+      'BBB/bbb.js',
+      'BBB/bbb.txt',
+      'CCC/ccc.js',
+      'CCC/ccc.txt',
+      'CCC/DDD/ddd.js',
+      'CCC/DDD/ddd.txt' ], 'Not supplying a filter selects every file');
 }());
 
-//(function() {
-//   var readSync = require('../lib/readdir.js').readSync;
-//
-//   var everyFile = readSync('example_dir', '**/**.js');
-//   Assert.deepEqual(everyFile, [ 'example_dir/AAA/aaa.js',
-//      'example_dir/abc.js',
-//      'example_dir/BBB/bbb.js',
-//      'example_dir/CCC/ccc.js',
-//      'example_dir/CCC/DDD/ddd.js' ]);
-//}());
+(function() {
+   var readSync = require('../lib/readdir.js').readSync;
+
+   var everyFile = readSync('example_dir', ['**/**.js']);
+   Assert.deepEqual(everyFile, ['AAA/aaa.js',
+      'BBB/bbb.js',
+      'CCC/ccc.js',
+      'CCC/DDD/ddd.js' ], 'double star slash requires sub-directory, js suffix requires file type');
+}());
+
+(function() {
+   var readSync = require('../lib/readdir.js').readSync;
+
+   var everyFile = readSync('example_dir', ['**.js']);
+   Assert.deepEqual(everyFile, [ 'AAA/aaa.js',
+      'abc.js',
+      'BBB/bbb.js',
+      'CCC/ccc.js',
+      'CCC/DDD/ddd.js' ], 'double star can have any number of sub-directories and a suffix requires file type');
+}());
+
+(function() {
+   var readSync = require('../lib/readdir.js').readSync;
+
+   var everyFile = readSync('example_dir', ['BBB/*']);
+   Assert.deepEqual(everyFile, ['BBB/bbb.js',
+      'BBB/bbb.txt' ], 'path prefix requires that directory, trailing star allows any file type');
+}());
+
+(function() {
+   var readSync = require('../lib/readdir.js').readSync;
+
+   var everyFile = readSync('example_dir', ['CCC/*']);
+   Assert.deepEqual(everyFile, ['CCC/ccc.js',
+      'CCC/ccc.txt'], 'Path prefix requires that directory, trailing single star ignores subsequent sub-direcotries');
+}());
+
+(function() {
+   var readSync = require('../lib/readdir.js').readSync;
+
+   var everyFile = readSync('example_dir', ['CCC/**']);
+   Assert.deepEqual(everyFile, ['CCC/ccc.js',
+      'CCC/ccc.txt',
+      'CCC/DDD/ddd.js',
+      'CCC/DDD/ddd.txt'], 'Double star will include sub-directories');
+}());
+
+(function() {
+   var readSync = require('../lib/readdir.js').readSync;
+
+   var everyFile = readSync('example_dir', ['*.txt']);
+   Assert.deepEqual(everyFile, ['abc.txt'], 'Single star ignores sub-directories and filename is a suffix');
+}());
