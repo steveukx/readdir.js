fb8d3211c6443c3154f3d58a99a8d5b546f3b513
Steve King <steve@mydev.co>
2020-02-08 15:15:20 +0000

    Add licence and fix path to published main/types

--- lib/read-dir-options.ts
+++ lib/read-dir-options.ts
@@ -37,3 +37,12 @@ export enum ReadDirOptions {
     IGNORE_ERRORS = 64,
 }
 
+
+export const ABSOLUTE_PATHS = ReadDirOptions.ABSOLUTE_PATHS;
+export const CASELESS_SORT = ReadDirOptions.CASELESS_SORT;
+export const CASE_SORT = ReadDirOptions.CASE_SORT;
+export const INCLUDE_DIRECTORIES = ReadDirOptions.INCLUDE_DIRECTORIES;
+export const INCLUDE_HIDDEN = ReadDirOptions.INCLUDE_HIDDEN;
+export const NON_RECURSIVE = ReadDirOptions.NON_RECURSIVE;
+export const IGNORE_ERRORS = ReadDirOptions.IGNORE_ERRORS;
+
